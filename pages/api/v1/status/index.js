8fe78cbe6f4ee0f4508c66cad8698767fecfa0fb
Lucas Martins <lukasousa70@gmail.com>
2026-03-16 16:56:25 -0300

    feat: create the `getHandler` function to show database status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,2 +1,39 @@
 import { createRouter } from "next-connect";
 import database from "infra/database";
+import controller from "infra/controller";
+
+const router = createRouter();
+
+router.get(getHandler);
+
+async function getHandler(request, response) {
+    const updatedAt = new Date().toISOString();
+
+    const databaseVersionRsult = await database.query("SHOW server_version;");
+
+    const databaseVersionValue = databaseVersionRsult.rows[0].server_version;
+
+    const databaseMaxConnectionsResult = await database.query("SHOW max_connections;");
+
+    const postgresConnection = databaseMaxConnectionsResult.rows[0].max_connections;
+
+    const databaseName = process.env.POSTGRES_DB;
+
+    const databaseOpenedConnectionsResult = await database.query({
+        text: "SELECT count(*) FROM pg_stat_activity WHERE datname = $1;",
+        values: [databaseName],
+    });
+
+    const databaseOpenedConnections = databaseOpenedConnectionsResult.rows[0].count;
+
+    response.status(200).json({
+        updatedAt: updatedAt,
+        dependencies: {
+            database: {
+                version: databaseVersionValue,
+                maxConnections: postgresConnection,
+                openedConnections: databaseOpenedConnections,
+            },
+        },
+    });
+}
