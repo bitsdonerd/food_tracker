3b5f2c5628bede60f3cebbdd6202eaa5611f51aa
Lucas Martins <lukasousa70@gmail.com>
2026-03-16 17:34:56 -0300

    test: add orchestrator to set up integration tests
    wait webserver status, clean database schemas, run pending migrations and create user faker for
    testes

---
+++ tests/orchestrator.js
@@ -0,0 +1,52 @@
+import retry from "async-retry";
+import database from "infra/database";
+import migrator from "models/migrator";
+import user from "models/user";
+import { faker } from "@faker-js/faker";
+
+async function waitForAllServices() {
+    await waitForWebServer();
+
+    async function waitForWebServer() {
+        return retry(fetchStatusPage, {
+            retries: 100,
+            maxTimeout: 1000,
+        });
+
+        async function fetchStatusPage() {
+            const response = await fetch("http://localhost:3000/api/v1/status");
+
+            if (response.status !== 200) {
+                throw Error(`HTTP error: ${response.status}`);
+            }
+        }
+    }
+}
+
+async function clearDatabase() {
+    await database.query("drop schema public cascade; create schema public;");
+}
+
+async function runPendingMigrations() {
+    await migrator.runPendingMigrations();
+}
+
+async function createUser() {
+    return user.create({
+        username:
+            userObject.username || faker.internet.username().replace(/[_.-]/g, ""),
+        email:
+            userObject.email || faker.internet.email(),
+        password:
+            userObject.password || "validpassword",
+    });
+}
+
+const orchestrator = {
+    waitForAllServices,
+    clearDatabase,
+    runPendingMigrations,
+    createUser
+};
+
+export default orchestrator;
