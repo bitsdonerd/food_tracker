783593ece88a4ad206cf719dda47e07516b04674
Lucas Martins <lukasousa70@gmail.com>
2026-03-16 16:39:31 -0300

    feat: create controller for `errorHandlers`

---
+++ infra/controller.js
@@ -0,0 +1,38 @@
+import {
+    InternalServerError,
+    MethodNotAllowedError,
+    ForbiddenError,
+    ValidationError,
+    NotFoundError,
+    UnauthorizedError
+} from "./errors";
+
+function onNoMatchHandler(req, res) {
+    const publicErrorObject = new MethodNotAllowedError();
+    res.status(publicErrorObject.statusCode).json(publicErrorObject);
+}
+
+function onErrorHandler(error, req, res) {
+    if (
+        error instanceof MethodNotAllowedError ||
+        error instanceof ForbiddenError ||
+        error instanceof ValidationError ||
+        error instanceof NotFoundError ||
+        error instanceof UnauthorizedError
+    ) {
+        return res.status(error.statusCode).json(error);
+    }
+
+    const publicErrorObject = new InternalServerError({ cause: error });
+    console.error(publicErrorObject)
+    res.status(publicErrorObject.statusCode).json(publicErrorObject);
+}
+
+const controller = {
+    errorHandlers: {
+        onNoMatch: onNoMatchHandler,
+        onError: onErrorHandler,
+    },
+};
+
+export default controller;
